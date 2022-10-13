c355e124f1bf0e99c07f953107b1cac7f6fc86a7
Julius Caezar <julius.caezar25@gmail.com>
2022-10-13 13:33:30 -0400

    import Meta and StoryObj from storybook/react

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
 import { Text } from "./Text";
 
 export default {
   title: 'Components/Text',
   component: Text,
-}
+} as Meta
 
 export const Default = {}
