735e86083cde0cab1adb92f12966fafb842f11e4
Julius Caezar <julius.caezar25@gmail.com>
2022-10-13 12:07:23 -0400

    creting the project colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'gray-900': '#121214',
+        'gray-800': '#282824',
+        'gray-400': '#7c7c8a',
+        'gray-200': '#c4c4cc',
+        'gray-100': '#e1e1e6',
+
+        'cyan-500': '#81d8f7',
+        'cyan-200': '#9BE1FB'
+      },
       fontFamily: {
         sans: 'Inter, sans-serif'
       }
