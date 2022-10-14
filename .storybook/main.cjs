1ea217a36165154800eda829e89e72cbc79405e5
Julius Caezar <julius.caezar25@gmail.com>
2022-10-14 19:23:21 -0400

    coment viteFInal

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,11 +15,12 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) => {
+  /*viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/ignite-lab-design-system/'
     }
 
     return config
   }
+  */
 }
